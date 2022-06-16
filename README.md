35e22d56dfaae721af458e856ec5bb3f4c26e44f
al <alenalexkuz@mail.ru>
2022-06-16 15:15:40 +0300

    + loging

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ Before running Flask needs to be told how to import it, by setting the FLASK_APP
 You can run your first web application, with the following command:
 
 *(venv) $ flask run*
+
+Install Flask-WTF(it's used for web forms) in your virtual environment:
+
+*(venv) $ pip3 install flask-wtf*
+
+
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 
 # home page
@@ -19,3 +20,14 @@ def index():
     ]
     page = render_template('index.html', title='Home', user=user, posts=posts)
     return page
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():  # receiving login request
+        flash(f'Login request form user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect('/index')
+    # login page
+    return render_template('login.html', title='Sing In', form=form)
+
